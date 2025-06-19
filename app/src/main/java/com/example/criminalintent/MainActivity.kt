9a6544cb558af2be83ff8b61a017a1a952c7801e
SunriseXDD <mikkyrock465@gmail.com>
2025-06-19 20:47:16 +0700

    Завершены главы 10 и 11

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
         setContentView(R.layout.activity_main)
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
+
         if (currentFragment == null)
         {
             val fragment = CrimeListFragment.newInstance()
