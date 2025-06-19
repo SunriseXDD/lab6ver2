ff37fa9a404f4bc62998e8d255a43994947d4df4
SunriseXDD <mikkyrock465@gmail.com>
2025-06-19 17:50:23 +0700

    Завершена глава 8

--- app/src/main/java/com/sample/criminalintent/Crime.kt
+++ app/src/main/java/com/sample/criminalintent/Crime.kt
@@ -3,4 +3,8 @@ package com.sample.criminalintent
 import java.util.Date
 import java.util.UUID
 
-data class Crime(val id: UUID = UUID.randomUUID(), var title: String = "", var date: Date = Date(), var isSolved: Boolean = false)
+data class Crime(val id: UUID =
+                     UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false)
